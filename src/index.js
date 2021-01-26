09cf2d71b4b0b6c1ec6d111ab71fb9752949ac8b
Krishna Nikhil Vedurumudi <krveduru@microsoft.com>
2021-01-26 22:43:20 +0530

    Added Basic Auth support

--- src/index.js
+++ src/index.js
@@ -7,13 +7,33 @@ const app = express()
 
 app.use(bodyParser.json());
 
-//  Connect all our routes to our application
-app.use('/todo', todoRoutes);
-
 // Mongo Connection
 var HOST_NAME = 'localhost';
 var DATABASE_NAME = 'demo';
 mongoose.connect('mongodb://' + HOST_NAME + '/' + DATABASE_NAME);
+
+// Authentication
+app.use((req, res, next) => {
+    // authentication middleware
+    const auth = {login: 'admin', password: 'admin1234'} // change this
+  
+    // parse login and password from headers
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+  
+    // Verify login and password are set and correct
+    if (login && password && login === auth.login && password === auth.password) {
+      // Access granted...
+      return next();
+    }
+  
+    // Access denied...
+    res.set('WWW-Authenticate', 'Basic realm="401"');
+    res.status(401).send('Authentication required.');
+});
+
+//  Connect all our routes to our application
+app.use('/todo', todoRoutes);
 
 const port = 3000
 
